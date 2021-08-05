e529a5e7700b4d28ca0e93478eca23ed351d895a
GMDEEP <stewartjgreen@gmail.com>
2021-08-05 21:52:02 +0000

    Updated People Page with foundation

--- src/js/views/PeoplePage.js
+++ src/js/views/PeoplePage.js
@@ -3,5 +3,32 @@ import React from "react";
 export function PeoplePage() {
 	const [color, setColor] = React.useState("green");
 
-	return <div>People Page</div>;
+	return (
+		<div className="row">
+			<div className="col-sm-6">
+				<div className="card">
+					<div className="card-body">
+						<img
+							src="https://via.placeholder.com/800x600.png?text=Stewart"
+							className="card-img-top"
+							alt="..."
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="col-sm-6">
+				<div className="card">
+					<div className="card-body">
+						<h5 className="card-title">Special title treatment</h5>
+						<p className="card-text">
+							With supporting text below as a natural lead-in to additional content.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
