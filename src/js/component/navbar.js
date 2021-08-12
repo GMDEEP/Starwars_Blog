bbe4a4f0abe7089d098d92edd878771414fb06fd
GMDEEP <stewartjgreen@gmail.com>
2021-08-12 14:49:09 +0000

    center navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -9,13 +9,13 @@ export const Navbar = () => {
 	const favorites = useContext(FavoriteContext);
 
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="d-flex justify-content-center navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">
+				<span className="d-flex justify-content-center navbar-brand mb-0 h1">
 					<img src="https://th.bing.com/th/id/OIP.x5pcPIJR6arF1M6EFTGMLgHaFj?pid=ImgDet&rs=1" />
 				</span>
 			</Link>
-			<div className="ml-auto">
+			{/* <div className="ml-auto">
 				<Link to="/people">
 					<button className="btn btn-primary">Galaxy&apos;s Characters</button>
 				</Link>
@@ -34,8 +34,8 @@ export const Navbar = () => {
 				<Link to="/vehicle/:id">
 					<button className="btn btn-primary">Vehicle Details</button>
 				</Link>
-			</div>
-			<div className="dropdown">
+			</div> */}
+			<div className="d-flex justify-content-center dropdown">
 				<button
 					onClick={e => {
 						if (showMenu == "") {
@@ -49,7 +49,7 @@ export const Navbar = () => {
 					id="dropdownMenuButton1"
 					dataBsToggle="dropdown"
 					ariaExpanded="false">
-					Dropdown button
+					Favorites List
 				</button>
 				<ul className={"dropdown-menu" + showMenu} ariaLabelledBy="dropdownMenuButton1">
 					{favorites.favoriteArray.map((fav, index) => {
